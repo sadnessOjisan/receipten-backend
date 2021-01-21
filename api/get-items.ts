532f9e603f3d8ef7733d5698447c095e118c7b44
sadness_ojisan <sadness.ojisan@gmail.com>
2021-01-21 18:40:24 +0900

    add post endpoint

--- api/get-items.ts
+++ api/get-items.ts
@@ -1,7 +1,5 @@
 import { NowRequest, NowResponse } from "@vercel/node";
-import { ItemPostBody } from "../type";
 
 export default (req: NowRequest, res: NowResponse) => {
-  const body = req.body as ItemPostBody;
-  return res.json({ body });
+  return res.json({ message: "Hello World" });
 };
---
+++ type.ts
@@ -0,0 +1,6 @@
+export interface Item {
+  itemName: string;
+  itemPrice: number;
+}
+
+export type ItemPostBody = Item[];
