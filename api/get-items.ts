baf0abeca5242755b71e5069fbcd74ce51a382a9
sadness_ojisan <sadness.ojisan@gmail.com>
2021-01-22 20:19:50 +0900

    get data

--- api/get-items.ts
+++ api/get-items.ts
@@ -1,5 +1,15 @@
-import { NowRequest, NowResponse } from "@vercel/node";
+import "../infra/firebase";
 
-export default (req: NowRequest, res: NowResponse) => {
-  return res.json({ message: "Hello World" });
+import { NowRequest, NowResponse } from "@vercel/node";
+import admin from "firebase-admin";
+import dotenv from "dotenv";
+
+const db = admin.firestore();
+
+export default async (req: NowRequest, res: NowResponse) => {
+  const { id } = req.query;
+  if (typeof id !== "string") return res.json("error");
+  const ref = await db.collection("item").doc(id);
+  const data = await (await ref.get()).data();
+  return res.json({ data });
 };
