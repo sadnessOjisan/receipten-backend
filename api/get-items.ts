1696a5a18ed23dbddd23971e97156c1d6cc53384
sadness_ojisan <sadness.ojisan@gmail.com>
2021-01-25 19:11:19 +0900

    add sum

--- api/get-items.ts
+++ api/get-items.ts
@@ -6,10 +6,17 @@ import dotenv from "dotenv";
 
 const db = admin.firestore();
 
+type D = { itemPrice: string; itemName: string }[];
+
 export default async (req: NowRequest, res: NowResponse) => {
   const { id } = req.query;
   if (typeof id !== "string") return res.json("error");
   const ref = await db.collection("item").doc(id);
-  const data = await (await ref.get()).data();
-  return res.json({ ...data });
+  const data: D = (await (await ref.get()).data()) as any;
+  const prices = data.map((d) => ({
+    ...d,
+    priceNum: parseInt(d.itemPrice.replace("￥", "").replace(/,/g, "")),
+  }));
+  const sum = prices.reduce((prev, current) => prev + current.priceNum, 0);
+  return res.json({ ...data, sum });
 };
